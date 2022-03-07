60e2ddd1f3705de5ce5a69fe523d3a5231262613
weberlacop <webergaia@id.uff.br>
2022-03-07 15:32:43 -0300

    Interrogador LPG V2

---
+++ 3. Interrogador LPG/2. Interrogador_LPG_V2/Interrogador_LPG_V2.m
@@ -0,0 +1,13 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                   UNIVERSIDADE FEDERAL FLUMINENSE                       %
+%                  LABORATÓRIO DE COMUNICAÇÕES ÓTICAS                     %
+%    DESENVOLVIMENTO DE INSTRUMENTO PARA INTERROGAÇÃO DE SENSORES ÓTICOS  %
+%               PROF. DR. ANDRÉS PABLO LOPEZ BARBERO                      %
+%                    WEBER DE SOUZA GAIA FILHO                            %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Variáveis de Limpeza
+clc;
+close all
+clear all
+%% CARREGANDO A AQUISIÇÃO DE DADOS DO DAQ6216
+a = 33;
