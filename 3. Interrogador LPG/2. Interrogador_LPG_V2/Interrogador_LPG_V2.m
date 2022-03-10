65d382aebd6b7eb7a5a50cc27a89d2f6b7dd88ca
weberlacop <webergaia@id.uff.br>
2022-03-09 22:23:10 -0300

    Alterações 09/03/2022

--- 3. Interrogador LPG/2. Interrogador_LPG_V2/Interrogador_LPG_V2.m
+++ 3. Interrogador LPG/2. Interrogador_LPG_V2/Interrogador_LPG_V2.m
@@ -10,26 +10,25 @@ clc;
 close all
 clear all
 %% CARREGANDO A AQUISIÇÃO DE DADOS DO DAQ6216
-% load 'C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\7. Interrogador LPG\2. 1800Hz\1543nm\Distorção 6%\DAQ6216_1s_10800.mat'
-% load 'C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\7. Interrogador LPG\2. 1800Hz\1543nm\Distorção 6%\DAQ6216_2s_10800.mat'
-% load 'C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\7. Interrogador LPG\2. 1800Hz\1543nm\Distorção 6%\DAQ6216_1s_14400.mat'
-load 'C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\7. Interrogador LPG\2. 1800Hz\1543nm\Distorção 6%\DAQ6216_2s_14400.mat'
-
+% load 'C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\7. Interrogador LPG\2. 1800Hz\1542nm\Distorção 10%\DAQ6216_1s_10800.mat'
 % DAQ = volt1;
 % clear volt1
 
+% load 'C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\7. Interrogador LPG\2. 1800Hz\1542nm\Distorção 10%\DAQ6216_2s_10800.mat'
 % DAQ = volt2;
 % clear volt2
 
+% load 'C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\7. Interrogador LPG\2. 1800Hz\1542nm\Distorção 10%\DAQ6216_1s_14400.mat'
 % DAQ = volt3;
 % clear volt3
 
+load 'C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\7. Interrogador LPG\2. 1800Hz\1542nm\Distorção 10%\DAQ6216_2s_14400.mat'
 DAQ = volt4;
 clear volt4
 %% 
 t = (0:length(DAQ)-1)*(1/length(DAQ));                                      % Vetor para cálculo ddo tempo
 Fs = 10800;                                                                 % Frequência de amostragem
-% Fs = 14400;                                                               % Frequência de amostragem
+Fs = 14400;                                                               % Frequência de amostragem
 [P1,aux_fase,f_plot] = FFT(DAQ,Fs);
 %% 
 f = 1800;
@@ -43,9 +42,11 @@ plot(f_plot,10*log10(P1),'k')
 %% COEFICIENTES PARA OBTENÇÃO DAS RAÍZES
 Am = 0.44;                                                                  % Amplitude de modulação
 a = 0.01;                                                                   % Largura espectral da LPG
-Ad = 6;                                                                     % Porcentagem de distorção; File:DAQ_10800_1s_90.mat -mat
+Ad = 0.044;                                                                 % Porcentagem de distorção; File:DAQ_10800_1s_90.mat -mat
 Lc = 1540;                                                                  % Comprimento de onda LPG
 [A,B,C] = coeff(Am,a,Ad,H);
 %% OBTENÇÃO DO COMPRIMENTO DE ONDA DA LPG
 [Lr] = LambdaR(A,B,C,index2,aux2_fase,Lc);
-[Lr2] = LambdaR2(A,B,C,index2,aux2_fase,Lc);
+% [Lr2] = LambdaR2(A,B,C,index2,aux2_fase,Lc);
+%% 
+[c,percent] = THD(DAQ,Fs);
--- 3. Interrogador LPG/2. Interrogador_LPG_V2/coeff.m
+++ 3. Interrogador LPG/2. Interrogador_LPG_V2/coeff.m
@@ -1,17 +1,21 @@
-function [A,B,C] = coeff(Am,a,Ad,H)
+function [B,C,D] = coeff(Am,a,Ad,H)
 %COEFICIENTES PARA CÁLCULO DAS RAÍZES
 %   Equações utilizadas foram do papper do vicente
+k13 = -4*a^2*Ad.^2.*Am-2*a^2*Am.^3; 
+k12 = -4*Ad.*a.*Am;                 
+k11 = -4.*Am+6*a.*Ad.^2.*Am+3*a.*Am.^3;
 k10 = 2*Ad.*Am;
-k11 = -4.*Am + 6*a.*Ad.^2.*Am + 3*a.*Am.^3;
-k12 = -4*Ad.*a.*Am; 
-k13 = -4*a^2*Ad.^2.*Am - 2*a^2*Am.^3; 
-                
-k20 = Am.^2;
-k21 = -4*Ad + 3*a*Ad.^3 + 6*a*Ad.*Am.^2;
-k22 = -2*a*Am.^2;
-k23 = -2*a^2*Ad.^3 - 4*a^2*Ad.*Am.^2;
 
-A = k22*H-k12;
-B = k21*H-k11;
-C = k20*H-k10;
+k23= -2*a^2*Ad.^3-4*a^2*Ad.*Am.^2;
+k22 = -2*a*Am.^2;
+k21 = -4*Ad+3*a*Ad.^3+6*a*Ad.*Am.^2;
+k20 = Am.^2 ;
+
+% A = k23.*H-k13;
+B = k22.*H-k12;
+C = k21.*H-k11;
+D = k20.*H-k10;
+
+s2 = (-C + sqrt(C.^2 - 4*B.*D))./(2*B);
+s1 = (-C - sqrt(C.^2 - 4*B.*D))./(2*B);
 end
