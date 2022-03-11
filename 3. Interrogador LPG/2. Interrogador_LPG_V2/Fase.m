248759259599a60cb2de985281744ac0f8d06a98
weberlacop <webergaia@id.uff.br>
2022-03-11 00:26:55 -0300

    11/03/2022

--- 3. Interrogador LPG/2. Interrogador_LPG_V2/Fase.m
+++ 3. Interrogador LPG/2. Interrogador_LPG_V2/Fase.m
@@ -1,9 +1,9 @@
 function [aux5,aux6,aux2] = Fase(f,Fs,ph)
 %UNTITLED3 Summary of this function goes here
 L = length(ph);
-aux1 = ph;
+aux1 = angle(ph);       %Fase
 aux2 = aux1/L;
-aux3 = aux2(1:L/2+1);
+% aux3 = aux2(1:L/2+1);
 aux4 = length(ph);
 aux5 = ceil(((aux4*f)/(Fs/2)));
 aux6 = aux5*2;
