7353783073bd84d855ae69f7a6e3ac9ffff9a4ab
weberlacop <webergaia@id.uff.br>
2022-03-11 17:28:59 -0300

    Interrogador para 1800hz

--- 3. Interrogador LPG/2. Interrogador_LPG_V2/Interrogador_LPG_V2.m
+++ 3. Interrogador LPG/2. Interrogador_LPG_V2/Interrogador_LPG_V2.m
@@ -10,49 +10,47 @@ clc;
 close all
 clear all
 %% CARREGANDO A AQUISIÇÃO DE DADOS DO DAQ6216
-% load 'C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\7. Interrogador LPG\2. 1800Hz\1539nm\Distorção 10%\DAQ6216_1s_10800.mat'
+% load 'C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\7. Interrogador LPG\2. 1800Hz\1541nm\Distorção 10%\DAQ6216_1s_10800.mat'
 % DAQ = volt1;
 % clear volt1
 
-load 'C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\7. Interrogador LPG\2. 1800Hz\1539nm\Distorção 10%\DAQ6216_2s_10800.mat'
-DAQ = volt2;
-clear volt2
+% load 'C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\7. Interrogador LPG\2. 1800Hz\1541nm\Distorção 3%\DAQ6216_2s_10800.mat'
+% DAQ = volt2;
+% clear volt2
 
-% load 'C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\7. Interrogador LPG\2. 1800Hz\1539nm\Distorção 9%\DAQ6216_1s_14400.mat'
+% load 'C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\7. Interrogador LPG\2. 1800Hz\1541nm\Distorção 10%\DAQ6216_1s_14400.mat'
 % DAQ = volt3;
 % clear volt3
 
-% load 'C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\7. Interrogador LPG\2. 1800Hz\1539nm\Distorção 10%\DAQ6216_2s_14400.mat'
+% load 'C:\Users\weber\Desktop\Dissertação - Simulador e  Interrogador LPG\3. Arquivos MATLAB\7. Interrogador LPG\2. 1800Hz\1541nm\Distorção 3%\DAQ6216_2s_14400.mat'
 % DAQ = volt4;
 % clear volt4
-d=10;
+
+d = 3;
 %% 
 t = (0:length(DAQ)-1)*(1/length(DAQ));                                      % Vetor para cálculo ddo tempo
 Fs = 10800;                                                                 % Frequência de amostragem
-% Fs = 14400;                                                                 % Frequência de amostragem
+Fs = 14400;                                                                 % Frequência de amostragem
 [P1,aux_fase,f_plot] = FFT(DAQ,Fs);
 %% 
 f = 1800;
 [index1, index2,aux2_fase] = Fase(f,Fs,aux_fase(1:length(P1)));
-%% 
+%% 10800
 % H1 = abs(P1(index1));
 % H2 = abs(P1(index2-2));
 
-H1 = abs(P1(index1-1));
-H2 = abs(P1(index2-4));
-%% 
+% H1 = abs(P1(index1-1));
+% H2 = abs(P1(index2-4));
+%% 14400
 % H1 = abs(P1(index1-1));
 % H2 = abs(P1(index2-3));
 
-% H1 = abs(P1(index1-2));
-% H2 = abs(P1(index2-6));
+H1 = abs(P1(index1-2));
+H2 = abs(P1(index2-6));
 %% 
 H = H1/H2;
 dbH1 = 10*log10(H1);                                                        % Transformação para dB (H1)
 dbH2 = 10*log10(H2);                                                        % Transformação para dB (H2)
-plot(f_plot,10*log10(P1),'k')
-grid on 
-grid minor
 %% COEFICIENTES PARA OBTENÇÃO DAS RAÍZES
 Am = 0.44;                                                                  % Amplitude de modulação
 a = 0.01;                                                                   % Largura espectral da LPG
@@ -62,8 +60,13 @@ Lc = 1540;                                                                  % Co
 [B,C,D] = coeff(Am,a,Ad,H);
 %% OBTENÇÃO DO COMPRIMENTO DE ONDA DA LPG
 [Lr] = LambdaR(B,C,D,index2,aux2_fase,Lc);
-[Lr2] = LambdaR2(B,C,D,index2,aux2_fase,Lc);
+% [Lr2] = LambdaR2(B,C,D,index2,aux2_fase,Lc);
 %% 
 [c,percent] = THD(DAQ,Fs);
 %% 
 SNR = snr(DAQ,Fs,3);
+plot(f_plot,10*log10(P1),'k')
+title(['FFT para F_s: ',num2str(Fs),'sps c/ distorção de ',num2str(d),'%'])
+subtitle(['H1: ',num2str(dbH1),'(dB)         H2: ',num2str(dbH2),'(dB)      ','Lr: ',num2str(Lr),'nm'])
+grid on 
+grid minor
